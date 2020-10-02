e49501af9f20b3e893770bc2fff7963a528b7399
Stypox <stypox@pm.me>
2020-10-02 16:03:43 +0200

    Fix video detail controls visibility set inconsistently

--- app/src/main/java/org/schabi/newpipe/fragments/detail/VideoDetailFragment.java
+++ app/src/main/java/org/schabi/newpipe/fragments/detail/VideoDetailFragment.java
@@ -1276,25 +1276,16 @@ public class VideoDetailFragment extends BaseStateFragment<StreamInfo>
             }
         }
 
-        switch (info.getStreamType()) {
-            case LIVE_STREAM:
-            case AUDIO_LIVE_STREAM:
-                detailControlsDownload.setVisibility(View.GONE);
-                spinnerToolbar.setVisibility(View.GONE);
-                break;
-            default:
-                if (info.getAudioStreams().isEmpty()) {
-                    detailControlsBackground.setVisibility(View.GONE);
-                }
-                if (!info.getVideoStreams().isEmpty() || !info.getVideoOnlyStreams().isEmpty()) {
-                    break;
-                }
+        detailControlsDownload.setVisibility(info.getStreamType() == StreamType.LIVE_STREAM
+                || info.getStreamType() == StreamType.AUDIO_LIVE_STREAM ? View.GONE : View.VISIBLE);
+        detailControlsBackground.setVisibility(info.getAudioStreams().isEmpty()
+                ? View.GONE : View.VISIBLE);
 
-                detailControlsPopup.setVisibility(View.GONE);
-                spinnerToolbar.setVisibility(View.GONE);
-                thumbnailPlayButton.setImageResource(R.drawable.ic_headset_shadow);
-                break;
-        }
+        final boolean noVideoStreams =
+                info.getVideoStreams().isEmpty() && info.getVideoOnlyStreams().isEmpty();
+        detailControlsPopup.setVisibility(noVideoStreams ? View.GONE : View.VISIBLE);
+        thumbnailPlayButton.setImageResource(
+                noVideoStreams ? R.drawable.ic_headset_shadow : R.drawable.ic_play_arrow_shadow);
 
         if (autoPlayEnabled) {
             openVideoPlayer();
