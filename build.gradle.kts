RinZ27/NewPipe

Initial support for compose multiplatform
Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

build.gradle.kts
@@ -5,8 +5,13 @@
 
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.jetbrains.kotlin.kapt) apply false
+    alias(libs.plugins.jetbrains.kotlin.compose) apply false
+    alias(libs.plugins.jetbrains.kotlin.multiplatform) apply false
+    alias(libs.plugins.jetbrains.compose.multiplatform) apply false
+    alias(libs.plugins.jetbrains.compose.hotreload) apply false
     alias(libs.plugins.google.ksp) apply false
     alias(libs.plugins.jetbrains.kotlin.parcelize) apply false
     alias(libs.plugins.sonarqube) apply false

ShareASmile/NewPipeZing

build.gradle.kts
@@ -12,9 +12,15 @@ buildscript {
 
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.legacy.kapt) apply false
     alias(libs.plugins.google.ksp) apply false
+    alias(libs.plugins.jetbrains.kotlin.compose) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.jetbrains.kotlin.multiplatform) apply false
+    alias(libs.plugins.jetbrains.compose.multiplatform) apply false
     alias(libs.plugins.jetbrains.kotlin.parcelize) apply false
     alias(libs.plugins.jetbrains.kotlinx.serialization) apply false
     alias(libs.plugins.sonarqube) apply false
+    alias(libs.plugins.koin) apply false
 }